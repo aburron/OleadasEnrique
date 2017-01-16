b884c752a9001725f3a808046c67b4e66db72621
aburron Albatraes <aburrondev@gmail.com>
2017-01-16 23:41:16 +0100

    16/01/2017 (23:41)
    Implementaciones:
    -Campo de futbol y porterias
    -Script del balón
    ---------------------------------------------------------------
    Pendiente:
    *Arreglar fisicas del balón
    *Añadir Pikuro balón
    *Añadir Pikuro bellota
    *Añadir Pikuro arbol

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public GameObject ballRespawn;
+
+	void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            this.transform.position = ballRespawn.transform.position;
+        }
+    }
+}
